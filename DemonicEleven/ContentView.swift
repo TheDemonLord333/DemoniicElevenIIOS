f8ca10e3bb8ef5591c3bba36b95bff14336d33ae
TheDemonLord333 <thedemonlord333@gmail.com>
2026-07-27 04:32:24 +0200

    Initial Commit

---
+++ DemonicEleven/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemonicEleven
+//
+//  Created by David Martens on 27.07.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemonicEleven/DemonicElevenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemonicElevenApp.swift
+//  DemonicEleven
+//
+//  Created by David Martens on 27.07.26.
+//
+
+import SwiftUI
+
+@main
+struct DemonicElevenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
